2f511b419778941f36ee7bf8b2819c01864f657b
Priyanshu-Sahu1 <priyanshu.sahu@iiitg.ac.in>
2023-05-15 15:55:11 +0530

    Update parameter.py

--- InstagramDataScraper-main/Instagram_Hastag_DataScraper/parameter.py
+++ InstagramDataScraper-main/Instagram_Hastag_DataScraper/parameter.py
@@ -1,13 +1,7 @@
-#Instagram Credentials
-# Facebook_username = 'yudhishthirsharma6@gmail.com'
-# Facebook_password = 'Changepwd@1997'
-Instagram_username = "yudhishthirsharma11"
-Instagram_password = "Qazxsw@321"
-#url from which need to scrape data
-# URL= "https://www.facebook.com/groups/1225966920763001"
+#Instagram Credentials'
+Instagram_username = "Enter_Instagram_Username"
+Instagram_password = "Enter_Instagram_Password"
+#Hashtag from which need to scrape data
 Instagram_Hastag = "propertyforsale"
 #Enter the relative path of your chromedriver
 relative_path = "./chromedriver.exe"
-
-# #Enter number of scrolls you want.
-# noOfScrolls = 100
